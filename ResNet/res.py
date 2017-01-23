0a2c74c9f1e0a78a6eae8d58baf4d99305b3eb1a
bellatoris <bellatoris@snu.ac.kr>
2017-01-23 12:23:20 +0900

    [pytorch] ResNet implementation start

---
+++ ResNet/res.py
@@ -0,0 +1,14 @@
+from __future__ import print_function
+import os
+import math
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+import torch.utils.data
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+import torchvision.utils as vutils
+from torch.autograd import Variable
